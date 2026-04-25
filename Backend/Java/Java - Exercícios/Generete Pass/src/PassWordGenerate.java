7e5fe07c37b2305f191377d626ad391edee37696
Abdoulayé Eduardo <abdoulayeduardo.k@gmail.com>
2026-04-25 13:47:21 +0100

    Exercício gerador de senha foi completado

--- Backend/Java/Java - Exercícios/Generete Pass/src/PassWordGenerate.java
+++ Backend/Java/Java - Exercícios/Generete Pass/src/PassWordGenerate.java
@@ -8,29 +8,30 @@ public class PassWordGenerate {
         Scanner read = new Scanner(System.in);
         Random generate = new Random();
 
-        char [] letra = {
-                'A','B','C','D','E','F','G'
-                ,'H','I','J','K','L','M','N'
-                ,'O','P','Q','R','S','T','U'
-                ,'V','W','X','Y','Z'
-        };
+        char [] letra = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-        System.out.print("Por favor informe o número de caracteres que a senha de ter: ");
+        System.out.print("\nPor favor informe o número de caracteres que a senha de ter: ");
         short numberKey = read.nextShort();
-
 
         char [] keys = new char [numberKey];
 
-        String password = " ";
+        String password;
 
-        for(int i = 0; i<numberKey; i++){
-            keys[i] = letra[generate.nextInt(numberKey)];
+        if (numberKey>6) {
+
+            for (int i = 0; i < numberKey; i++) {
+                keys[i] = letra[generate.nextInt(numberKey)];
+            }
+
+            System.out.print("\n\nA sua senha é: ");
+
+            for (int i = 0; i < numberKey; i++) {
+                password = Character.toString(keys[i]);
+                System.out.print(password);
+            }
+            System.out.println("\n");
+        } else{
+            System.out.println("\n\nImpossivel ter uma uma senha segura com está quantidade de caracteres\n\n");
         }
-
-        for(int i = 0; i<numberKey; i++){
-            password = ""+Character.toString(keys[i]);
-        }
-
-        System.out.println("A sua senha é: "+password);
     }
 }
