d880dbda8b47446b8c7b3b8fbd8c7a36d66bd328
Abdoulayé Eduardo <abdoulayeduardo.k@gmail.com>
2026-05-03 23:15:30 +0100

    Gerador de Números em intervalos expecificos concluido

---
+++ Backend/Java/Java - Exercícios/Sorteio de Numero/src/RandomNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        short random = (short) (0 + Math.random() * (5-0));
+
+        System.out.println("\nEstou a pensar em um número entre 0 e 5");
+        System.out.println("Digite o valor do teu palpite\n");
+
+        System.out.print("Valor: ");
+        short palpite = scanner.nextShort();
+
+        System.out.println("");
+        if (palpite==random){
+            System.out.println("Acertou Encheio!");
+        }else {
+            if (palpite>5 || palpite<0){
+                System.out.println("Dica: Digitaste Um Valor Fora Do Intervalo.");
+            }else {
+                System.out.println("Você Errou!");
+            }
+        }
+    }
+}
